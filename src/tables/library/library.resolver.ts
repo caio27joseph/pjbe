28b4262a55aa3d6a6051735bd925e688a9f4ab0b
Caio Joseph <caio27joseph@gmail.com>
2024-04-14 14:49:56 -0300

    Remove graphql-subscriptions and graphql-ws dependencies

--- src/tables/library/library.resolver.ts
+++ src/tables/library/library.resolver.ts
@@ -4,7 +4,6 @@ import {
   Mutation,
   Args,
   Int,
-  Subscription,
   ResolveField,
 } from '@nestjs/graphql';
 
@@ -19,32 +18,23 @@ import {
 } from './library.inputs';
 import { WhereInput } from 'src/graphql/dto/WhereOne.input';
 import { InjectRepository } from '@nestjs/typeorm';
-import { PubSub } from 'graphql-subscriptions';
 import { UseGuards } from '@nestjs/common';
 import { GqlAuthGuard } from 'src/auth/guards/gql-jwt.guard';
-import { LibraryEvent } from './library.events';
 
 @UseGuards(GqlAuthGuard)
 @Resolver(() => Library)
 export class LibraryResolver {
-  private readonly pubSub: PubSub;
   constructor(
     @InjectRepository(Library)
     private readonly libraryRepo: Repository<Library>,
   ) {
-    this.pubSub = new PubSub();
   }
 
   @Mutation(() => Library)
   async createLibrary(@Args('input') input: CreateLibraryInput) {
     const entity = this.libraryRepo.create(input);
     const library = await this.libraryRepo.save(entity);
-    this.pubSub.publish(
-      LibraryEvent.EVENT,
-      LibraryEvent.toPayload({
-        created: library,
-      }),
-    );
+
     return library;
   }
 
@@ -58,12 +48,7 @@ export class LibraryResolver {
       ...library,
       ...input,
     });
-    this.pubSub.publish(
-      LibraryEvent.EVENT,
-      LibraryEvent.toPayload({
-        updated: library,
-      }),
-    );
+
     return library;
   }
 
@@ -74,32 +59,13 @@ export class LibraryResolver {
   ) {
     const library = await this.libraryRepo.findOneByOrFail(where);
     library.addDirectory(input);
-    this.pubSub.publish(
-      LibraryEvent.EVENT,
-      LibraryEvent.toPayload({
-        updated: library,
-      }),
-    );
-    console.log(library);
-    return this.libraryRepo.save(library);
-  }
 
-  @Subscription((returns) => LibraryEvent, {
-    name: LibraryEvent.EVENT,
-  })
-  events() {
-    return this.pubSub.asyncIterator(LibraryEvent.EVENT);
+    return this.libraryRepo.save(library);
   }
 
   @Mutation(() => Library)
   async removeLibrary(@Args('where') where: WhereInput) {
     const library = await this.libraryRepo.findOneByOrFail(where);
-    this.pubSub.publish(
-      LibraryEvent.EVENT,
-      LibraryEvent.toPayload({
-        removed: library,
-      }),
-    );
     const result = this.libraryRepo.delete({
       id: library.id,
     });
